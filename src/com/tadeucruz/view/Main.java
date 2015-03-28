6a27f3ffa6277b0b80ee862b3dae6bb4c32ba5bd
tadeucruz <tadeucruz@gmail.com>
2015-03-27 22:46:29 -0300

    Melhores a informação aparesentada na tela

--- src/com/tadeucruz/view/Main.java
+++ src/com/tadeucruz/view/Main.java
@@ -5,8 +5,9 @@ import com.tadeucruz.control.MainControler;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Iniciando");
+        System.out.println("Iniciando organização da musica");
         MainControler mc = new MainControler();
         mc.organizeMusic("/mnt/dados/MusicaOld/", "/mnt/dados/Musica/");
+        System.out.println("Teminou a organização da musica");
     }
 }
